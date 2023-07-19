41d01198540e68975570c370dfaedfd58b71c4e9
Tommaso De Ponti <tommasodeponti@zohomail.eu>
2023-07-19 12:37:30 +0200

    updating to preview 10

--- test_contracts/simple_log/src/lib.rs
+++ test_contracts/simple_log/src/lib.rs
@@ -1,12 +1,17 @@
 #![no_std]
 
-use soroban_sdk::{contractimpl, Env};
+use soroban_sdk::{contract, contractimpl, Env};
 
+#[contract]
 pub struct Contract;
 
 #[contractimpl]
 impl Contract {
     pub fn hello(env: Env) {
         env.events().publish(("hello", ), ("hello", "event"))
+    }
+
+    pub fn bump(env: Env) {
+        env.storage().instance().bump(100000000);
     }
 }
